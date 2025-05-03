f6bb3f2d6958bdb42d37b5133a927726db6a33d5
marayatdev <149398037+marayatdev@users.noreply.github.com>
2025-05-03 23:24:19 +0700

    feat: add deploy with docker and nginx.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,17 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: Number(process.env.VITE_PORTS),
+    proxy: {
+      "/api": {
+        target: process.env.VITE_URL_ENDPOINT_API,
+        changeOrigin: true,
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
